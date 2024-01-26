24beffae0c73fee941e46a050e90e693d35db411
Skaistev <153711410+Skaistev@users.noreply.github.com>
2024-01-26 23:47:17 +0200

    papildomas

--- 7kyu.js
+++ 7kyu.js
@@ -13,3 +13,15 @@ function michaelPays(costs) {
 return parseFloat((costs<5?costs:costs/3<10?costs-(costs/3):costs-10).toFixed(2))
                 
 }
+
+function hhh (a){
+    let el = "";
+    for (let i = 1; i <= a-1; i++) {
+        for (let j = 1; j <= a; j++) {
+            el += "+";
+         }
+         el += "\n"
+    }
+    return el + '+'.repeat(a)
+    }
+    console.log(hhh(8))
