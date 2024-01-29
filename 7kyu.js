f8bea19c7dee678af291ef918253453228bd7a3e
Skaistev <153711410+Skaistev@users.noreply.github.com>
2024-01-29 23:01:17 +0200

    papildytas

--- 7kyu.js
+++ 7kyu.js
@@ -14,7 +14,7 @@ return parseFloat((costs<5?costs:costs/3<10?costs-(costs/3):costs-10).toFixed(2)
                 
 }
 
-function hhh (a){
+function square (a){
     let el = "";
     for (let i = 1; i <= a-1; i++) {
         for (let j = 1; j <= a; j++) {
@@ -24,4 +24,25 @@ function hhh (a){
     }
     return el + '+'.repeat(a)
     }
-    console.log(hhh(8))
+    console.log(square(8));
+
+
+function move_zeros(arr,isRight) {
+let array = [];
+let array1 =[];
+for(i=0;i<arr.length;i++){
+    if(isRight===true||isRight===undefined){
+    const f = arr[i]>0? array.push(arr[i]):array1.push(arr[i]);
+}if (isRight===false){
+    const f = arr[i]>0? array1.push(arr[i]):array.push(arr[i]);
+}
+}
+const f = array.concat(array1);
+return f
+ }
+ 
+
+console.log(move_zeros([12, 0, 10, 0, 8, 12, 7, 6, 0, 4, 10, 12, 0], true), [12, 10, 8, 12, 7, 6, 4, 10, 12, 0, 0, 0, 0]);
+console.log(move_zeros([12, 0, 10, 0, 8, 12, 7, 6, 0, 4, 10, 12, 0], false), [0, 0, 0, 0, 12, 10, 8, 12, 7, 6, 4, 10, 12]);
+console.log(move_zeros([12, 0, 10, 0, 8, 12, 7, 6, 0, 4, 10, 12, 0]), [12, 10, 8, 12, 7, 6, 4, 10, 12, 0, 0, 0, 0]);
+
